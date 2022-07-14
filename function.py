62c56980e1a258868983c3574881e47931b496d0
AirCods <airscods@gmail.com>
2022-07-14 18:17:03 +0100

    create function for download video

--- function.py
+++ function.py
@@ -1,7 +1,7 @@
 import requests
 
 img_url = 'https://proprikol.ru/wp-content/uploads/2019/12/kartinki-pro-tuman-49.jpg'
-
+video_url = 'https://openseauserdata.com/files/9f1e2635a6679000fb1b8db992d3552f.mp4#t=0.001'
 
 def download_img(url=''):
 
@@ -17,8 +17,23 @@ def download_img(url=''):
         print(response_error)
 
 
+def download_video(url=''):
+
+    try:
+        response = requests.get(url=url)
+
+        with open('req_video.mp4', 'wb') as file:
+            file.write(response.content)
+
+        return 'Video successfully downloaded!'
+
+    except Exception as response_error:
+        print(response_error)
+
+
 def main():
     print(download_img(url=img_url))
+    print(download_video(url=video_url))
 
 
 if __name__ == "__main__":
